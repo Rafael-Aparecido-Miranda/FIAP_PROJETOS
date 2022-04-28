5b3d93710d46abaab0324f8dbaa998419616f707
Rafael Aparecido Miranda de Oliveira <rafael.aparecido.miranda@hotmail.com>
2022-04-28 13:46:32 -0300

    comparando funcoes

---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+let a = "Hello";
+let b = "World";
+let d = Math.floor(Math.random()*100);
+function minhaPrimeiraFuncao()
+{
+    
+    let c = Math.floor(Math.random() * 1000);
+    
+    return `${c} / ${d} = ${c/d}`
+}
+console.log(minhaPrimeiraFuncao());

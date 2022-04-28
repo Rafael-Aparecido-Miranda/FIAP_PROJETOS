1e9f107ad89848c7a5c3358444717bdffa50f73e
Rafael Aparecido Miranda de Oliveira <rafael.aparecido.miranda@hotmail.com>
2022-04-28 13:49:34 -0300

    estudo de variaveis

---
+++ js/variaveis.js
@@ -0,0 +1,6 @@
+//escrever variaveis as tres formas são var(tem muito mais abrangencia que uma let) let const(é uma constante)
+
+//number, string,booleano,array,objeto
+//var texts=[`Meu arry de numeros é ${nums}`]  isso é interpolação
+
+

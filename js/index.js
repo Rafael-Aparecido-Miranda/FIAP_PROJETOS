1b397429e39c837cc8d4d2dd23875e98c0923b27
Rafael Aparecido Miranda de Oliveira <rafael.aparecido.miranda@hotmail.com>
2022-04-28 17:10:46 -0300

    criado vetores para adicionar filmes

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let listaDeFilmes = [
+    'https://br.web.img3.acsta.net/medias/nmedia/18/90/95/96/20122166.jpg',
+    'https://br.web.img2.acsta.net/c_310_420/pictures/13/12/18/20/56/273919.jpg',
+    'https://m.media-amazon.com/images/I/517p+iKIK2L._AC_SX342_.jpg',
+    'https://media.fstatic.com/VcQode_XESjYEZrHOPsF-iR7c5o=/290x478/smart/media/movies/covers/2009/08/c912c5113e56bf032aa9beee3c1de24d.jpg',
+    'https://upload.wikimedia.org/wikipedia/pt/thumb/8/82/Pulp_Fiction_cover.jpg/230px-Pulp_Fiction_cover.jpg'
+];
+
+let listaDeTitulos = [
+    'Clube da Luta',
+    'Efeito Borboleta',
+    'Donnie Darko',
+    'Dagon',
+    'Pulp Fiction'
+];
